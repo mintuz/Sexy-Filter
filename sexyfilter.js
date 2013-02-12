cd2f441c6f97bf29be1bfe54b3a212a85dcf8147
Adam Bulmer <mintuz1990@gmail.com>
2013-02-12 20:59:04 +0000

    Initial Commit, Works like a charm baby.

---
+++ sexyfilter.js
@@ -0,0 +1,40 @@
+/*
+ * Sexy Filter 0.1
+ * @Author Adam Bulmer
+ * Primative Filter Plugin for image galleries, This will one day be a beast.
+ * Also Its my first plugin so i'm happy :)
+ */
+
+( function( $ ) {
+	  
+	  $.fn.SFilter = function( o ) {
+	  
+	  	//The Defaults
+	  	var s = $.extend( {
+	  		'type' : 'fade',
+	  		'container' : '.portfolio',
+	  		'low_opacity' : 0.2,
+	  		'high_opacity' : 1.0,
+	  		'speed' : 150
+
+	  	}, o );
+
+	  	//On Click do its magic.
+	    $( this ).click( function() {
+
+			var data = $( this ).data( 'shoot' );
+			var highlight = $( s.container ).find( '.' + data );
+
+			if( s.type == 'fade' ) {
+				
+				$( highlight ).animate( { opacity: s.high_opacity }, s.speed ).parents( s.container ).find( 'img:not(.' + data + ')' ).animate( { opacity: s.low_opacity }, s.speed );
+			
+			}
+
+			return false;
+
+		});
+
+	  };
+
+} )( jQuery );
